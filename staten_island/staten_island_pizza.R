0b8b93253817da7ecb6cef773a8ec4ea50ce06e5
Tanya Shapiro <44143974+tashapiro@users.noreply.github.com>
2022-08-10 12:20:46 -0400

    Update staten_island_pizza.R

--- staten_island/staten_island_pizza.R
+++ staten_island/staten_island_pizza.R
@@ -1,15 +1,20 @@
+library(tidyverse)
+library(ggtext)
+library(ggrepel)
 library(osmdata)
-library(ggplot2)
 library(sf)
-library(dplyr)
-library(raster)
-library(rgeos)
+library(sysfonts)
+library(showtext)
+
+sysfonts::font_add_google("galada")
+showtext_auto()
 
 #get coord plot for Staten Island
 coords<-getbb("Staten Island")
 
 #get polygon shape
-shape <- getbb("Staten Island", format_out = "sf_polygon")
+shape <- getbb("Staten Island, New York",  format_out = "sf_polygon")
+shape<-shape[3,]
 
 #gather main streets
 streets <- coords%>%
@@ -19,8 +24,10 @@ streets <- coords%>%
                             "secondary", "tertiary")) %>%
   osmdata_sf()
 
+#eliminate streets outside of Staten Island shape
 streets<-streets$osm_lines%>%
-  st_intersection(shape$multipolygon)
+  st_intersection(shape)
+
 
 small_streets <-coords%>%
   opq()%>%
@@ -31,7 +38,8 @@ small_streets <-coords%>%
   osmdata_sf()
 
 small_streets<-small_streets$osm_lines%>%
-  st_intersection(shape$multipolygon)
+  st_intersection(shape)
+
 
 leisure <- coords%>%
   opq()%>%
@@ -39,70 +47,64 @@ leisure <- coords%>%
   osmdata_sf()
 
 leisure_poly<-leisure$osm_polygons%>%
-  st_intersection(shape$multipolygon)
+  st_intersection(shape)
 
-pizza_shops<-read.csv("si_pizza.csv")
 
-col_island<-'#2A628F'
-col_road<-'#46B3FF'
-col_water<-'#16324F'
+pizza_shops<-read.csv("https://raw.githubusercontent.com/tashapiro/30DayMapChallenge/main/staten_island/si_pizza.csv")
 
-library(ggrepel)
+title = "<span style='font-family:galada;font-size:22pt;'>**Slice of Staten Island**</span><br><br>
+<span style='font-size:10pt;'>Map of Staten Island pizzerias and their<br> respective Google review scores. Best pizzerias <br>with a minimum of 600 reviews  are labeled.</span>"
 
-ggplot() +
-  geom_sf(data=shape$multipolygon,
-          fill=col_island,
-          color=col_road,
-          size=.4)+
+pal_bg<-'#16324F'
+pal_shape<-'#2A628F'
+pal_road<-'#46B3FF'
+pal_font<-'white'
+pal_park<-'#54B66F'
+#plot
+ggplot()+
+  geom_sf(data=shape, color=pal_road, fill=pal_shape)+
   geom_sf(data = streets,
           inherit.aes = FALSE,
-          color = col_road,
+          color = pal_road,
           size = .4)+
   geom_sf(data = small_streets,
           inherit.aes = FALSE,
-          color = col_road,
+          color = pal_road,
           size = .2,
           alpha = .8)+
- geom_sf(data=leisure_poly%>%filter(leisure %in% c("nature_reserve","park","golf_course")),
-          fill="#52BF93",
-          color=col_island
+  geom_sf(data=leisure_poly%>%filter(leisure %in% c("nature_reserve","park","golf_course")),
+          fill=pal_park,
+          color=pal_shape
   )+
   geom_point(data=pizza_shops,
-          inherit.aes = FALSE, color = "white",
-          #fill="white",
-          aes(x=Longitude,y=Latitude, fill=review_avg),
-          alpha = 1, size =4, shape = 21,
-          stroke=0.6)+
+             inherit.aes = FALSE, color = "white",
+             aes(x=Longitude,y=Latitude, fill=review_avg),
+             alpha = 1, size =4, shape = 21,
+             stroke=0.6)+
   geom_label_repel(data=pizza_shops%>%filter(review_avg>=4.2 & reviews>=600),
-            aes(x=Longitude,y=Latitude, label=abbr), fill=col_water, color="white",
-            size=3.25,
-            family="Gill Sans")+
- # scale_fill_gradient(low = "white", high = "#F35B04", na.value = NA)+
+                   aes(x=Longitude,y=Latitude, label=abbr), fill=pal_bg, color="white",
+                   size=2.5)+
+  geom_richtext(aes(x=-74.08, y=40.515, label=title),
+                color="white", label.color = NA, fill = NA)+
+  scale_y_continuous(limits=c(40.49,40.66))+
+  scale_x_continuous(limits=c(-74.26,-74.04))+
   scale_fill_steps(n.breaks=6, low ="#EF3054", high="#FFE74C",
                    guide=guide_legend(override.aes = list(size=10)))+
-  guides(fill = 
-           guide_legend(title.position = "top",
+  guides(fill = guide_legend(title.position = "bottom",
                         title.hjust =0.5,
+                        title="Avg Google Review",
                         label.position = "bottom",
-                        nrow = 1)
-         )+
+                        nrow = 1))+
+  labs(caption="Source OpenStreetMap & Google | Chart @tanya_shapiro")+
   theme_void()+
   theme(
-    plot.background = element_rect(fill=col_water),
-    text=element_text(family="Gill Sans",color="white"),
-    plot.margin = unit(c(0, 2, 0.2, 2), "cm"),
-    plot.title =element_text(family="Gill Sans Bold",color="white",size=20, hjust=0.99, vjust= - 78),
-    plot.subtitle=element_text(size=14, hjust=0.92, vjust=-97.5),
-    plot.caption=element_text(size=10, margin=margin(t=10,b=5)),
-    legend.position = c(0.1,1)
-  )+
-  labs(title="Slice of Staten Island",
-       subtitle="Map of Staten Island Pizzerias",
-       caption= "Data from Google & OpenStreetMap | Chart @tanya_shapiro",
-       fill="Google Review Average"
+    text=element_text(color="white"),
+    plot.caption=element_text(color=pal_road, hjust=0.05),
+    plot.background = element_rect(fill=pal_bg, color=NA),
+    legend.title=element_text(size=9, face="bold", margin=margin(t=5)),
+    legend.position = c(0.79, 0.0475),
+    plot.margin = margin(t=20,b=20,l=20,r=20)
   )
-                         
 
-ggsave("staten_island.png")
-#ggsave("staten_island.png", width = 9.5, height = 10, units='in')                          
 
+#ggsave("pizzerias.png", height=8, width=8)
