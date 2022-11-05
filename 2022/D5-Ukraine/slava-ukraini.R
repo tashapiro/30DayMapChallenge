2d8e643a5973b4ec35ea675725999697b10a5199
Tanya Shapiro <44143974+tashapiro@users.noreply.github.com>
2022-11-05 15:28:54 -0400

    Add files via upload

--- 2022/D5-Ukraine/slava-ukraini.R
+++ 2022/D5-Ukraine/slava-ukraini.R
@@ -1,12 +1,10 @@
 library(tidyverse)
-library(rnaturalearth)
 library(sf)
 library(raster)
 library(ggtext)
 library(sysfonts)
 library(showtext)
-
-#fFONT SET UP
+library(spMaps)
 
 
 #download Kyiv Type (by Ukrainian font designer Dmytro Rastvort) font https://fontesk.com/kyiv-type-typeface/
@@ -19,13 +17,10 @@ showtext_auto()
 showtext::showtext_opts(dpi = 300)
 
 
+#map from spMaps
+outline<-getSpMaps(countries = "UKR")
+
 #Map Cropping based on Josh O'Brien's answer form Stack Overflow: https://stackoverflow.com/questions/13982773/crop-for-spatialpolygonsdataframe
-
-#get map outline of Ukraine
-outline<-rnaturalearth::ne_countries(scale = 10, type = "countries", continent = NULL,
-                      country = "Ukraine", geounit = NULL, sovereignty = NULL,
-                      returnclass = c("sp", "sf"))
-
 
 #create bounding boxes for top half and lower half
 box_top <- as(extent(22, 41, 49, 53), "SpatialPolygons")
@@ -43,6 +38,7 @@ pal_blue<-"#0057b7"
 pal_gold<-"#ffd700"
 pal_bg<-"#292E31"
 
+#plot title & caption
 title<-paste0(
   "<span style='font-family:defaultsans;font-size:15pt;color:white;'>**#30DayMapChallenge**</span><br>",
   "<p><span style='font-family:kyiv;font-size:10pt;color:#ffd700;'>Kyiv Type Tilting</span>",
@@ -54,6 +50,7 @@ caption<-paste0(
   "<span style='font-family:fb;color:grey80;font-size:8pt;'>&#xf09b;'</span>",
   "<span style='font-family:defaultsans;color:grey80;font-size:8pt;'> tashapiro</span>"
 )
+
 
 #PLOT
 ggplot()+
@@ -74,18 +71,20 @@ ggplot()+
   geom_text(mapping=aes(x=33.8, y=48.25, label="UKRAINI"), 
                   size=15, color=pal_blue, family="kyiv", fontface="bold")+
   #title
-  geom_textbox(mapping=aes(label=title, x=36, y=44.5), halign=1, box.size=NA, fill=NA,
+  geom_textbox(mapping=aes(label=title, x=37, y=43), halign=1, box.size=NA, fill=NA,
                width = unit(4, "inch"))+
   #caption
-  geom_textbox(mapping=aes(label=caption, x=23.5, y=44.1), halign=0.5, box.size=NA, fill=NA,
+  geom_textbox(mapping=aes(label=caption, x=22.65, y=42.5), halign=0.5, box.size=NA, fill=NA,
                width = unit(4, "inch"))+
   coord_map()+
-  scale_y_continuous(limits=c(43.5,53), expand=c(0,0))+
+  scale_y_continuous(limits=c(42,53), expand=c(0,0))+
+  scale_x_continuous(limits=c(19,44), expand=c(0,0))+
   theme_void()+
   theme(
     plot.background = element_rect(fill=pal_bg, color=pal_bg),
     plot.margin = margin(t=30, b=5)
   )
 
+
 #save plot
-ggsave("slava-ukraini.png", height=6, width=9, units="in")
+ggsave("slava-ukraini.png", height=5.3, width=9, units="in")
